e07f5eafebd825ba877e0482bfe1040f399cf7a2
Brian Jenkins <brianjenkins94@gmail.com>
2018-08-10 13:15:57 -0400

    Updated config.py

--- PrizmDoc-Python-Sample/config.py
+++ PrizmDoc-Python-Sample/config.py
@@ -1,9 +1,13 @@
 from argparse import ArgumentParser
+import subprocess
+
+process = subprocess.run("npm install", shell=True, check=True)
+
 _parser = ArgumentParser()
 
-_parser.add_argument("--port", default="5000",)
-_parser.add_argument("--pasUrl", default="localhost",)
-_parser.add_argument("--pasPort", default="3000",)
-_parser.add_argument("--apiKey", default="",)
+_parser.add_argument("--port", default="5000")
+_parser.add_argument("--pasUrl", default="localhost")
+_parser.add_argument("--pasPort", default="3000")
+_parser.add_argument("--apiKey", default="")
 
 args = _parser.parse_args()
