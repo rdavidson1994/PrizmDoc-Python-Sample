d6cf2901804cb61d8ca8709521665f10c7413db8
Brian Jenkins <brianjenkins94@gmail.com>
2018-08-20 14:57:25 -0400

    Debugging file upload

--- PrizmDoc-Python-Sample/services/viewing_session.py
+++ PrizmDoc-Python-Sample/services/viewing_session.py
@@ -6,27 +6,39 @@ from config import args as config
 
 blueprint = Blueprint("viewing_session", __name__)
 
-@blueprint.route("/viewingSession")
+@blueprint.route("/viewingSession", methods=("GET", "POST"))
 def viewing_session():
     if request.method == "GET":
         request_type = request.args.get("type")
 
-        out_json = {"source": {"type": request_type}}
+        post_json = {"source": {"type": request_type}}
 
         if request_type == "document":
-            out_json["source"]["fileName"] = request.args.get("document")
+            post_json["source"]["fileName"] = request.args.get("document")
         elif request_type == "url":
-            out_json["source"]["url"] = request.args.get("url")
+            post_json["source"]["url"] = request.args.get("url")
         elif request_type == "viewingPackage":
-            out_json["source"]["documentId"] = request.args.get("viewingPackage")
+            post_json["source"]["documentId"] = request.args.get("viewingPackage")
 
-        url = f"http://localhost:{config.port}/pas/ViewingSession"
+        post_url = f"http://localhost:{config.port}/pas/ViewingSession"
 
-        response = requests.post(url=url, json=out_json)
+        response = requests.post(url=post_url, json=post_json)
 
         viewing_session_id = json.loads(response.text)["viewingSessionId"]
 
         return redirect(f"/viewer?viewingSessionId={viewing_session_id}")
     elif request.method == "POST":
-        # TBD, gotta figure out that busboy stuff
-        raise NotImplementedError
+        post_json = {"source": {"type": "upload", "displayName": "upload"}}
+        post_url = f"http://localhost:{config.port}/pas/ViewingSession"
+
+        post_response = requests.post(url=post_url, json=post_json)
+
+        viewing_session_id = json.loads(post_response.text)["viewingSessionId"]
+
+        put_url = f"http://localhost:{config.port}/pas/ViewingSession/{viewing_session_id}/SourceFile"
+
+        print(request.files.getlist("uploadInput")[0].read())
+
+        requests.put(url=put_url, data={"upload": ("upload", request.files.getlist("uploadInput")[0].read(), "application/octet-stream")})
+
+        return redirect(f"/viewer?viewingSessionId={viewing_session_id}")
